5f52b054463ec4777c46a45449cc7a667c2df7ed
Venilton Falvo Júnior <venilton.falvo@gmail.com>
2015-05-21 00:14:02 -0300

    Update project 8th day

---
+++ FAL8/MyApplication/app/src/main/java/com/example/administrador/myapplication/controllers/material/ServiceOrderListActivity.java
@@ -0,0 +1,93 @@
+package com.example.administrador.myapplication.controllers.material;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.administrador.myapplication.R;
+import com.example.administrador.myapplication.models.entities.ServiceOrder;
+import com.example.administrador.myapplication.util.AppUtil;
+import com.melnykov.fab.FloatingActionButton;
+
+import java.util.List;
+
+public class ServiceOrderListActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+
+    public static final int REQUEST_CODE_ADD = 1;
+    public static final int REQUEST_CODE_EDIT = 2;
+    private RecyclerView mServiceOrders;
+    private ServiceOrderListAdapter mServiceOrdersAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_order_list_material);
+
+        this.bindElements();
+    }
+
+    private void bindElements() {
+        mServiceOrders = AppUtil.get(findViewById(R.id.recyclerViewServiceOrders));
+        mServiceOrders.setHasFixedSize(true);
+        mServiceOrders.setLayoutManager(new LinearLayoutManager(this));
+
+        final FloatingActionButton fabAdd = AppUtil.get(findViewById(R.id.fabAdd));
+        fabAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent goToAddActivity = new Intent(ServiceOrderListActivity.this, ServiceOrderActivity.class);
+                startActivityForResult(goToAddActivity, REQUEST_CODE_ADD);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final List<ServiceOrder> serviceOrders = ServiceOrder.getAll();
+        if (mServiceOrdersAdapter == null) {
+            mServiceOrdersAdapter = new ServiceOrderListAdapter(serviceOrders);
+            mServiceOrders.setAdapter(mServiceOrdersAdapter);
+        } else {
+            mServiceOrdersAdapter.setItens(serviceOrders);
+            mServiceOrdersAdapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_CODE_ADD) {
+                Toast.makeText(this, getString(R.string.msg_add_success), Toast.LENGTH_LONG).show();
+            } else if (requestCode == REQUEST_CODE_EDIT) {
+                Toast.makeText(this, getString(R.string.msg_edit_success), Toast.LENGTH_LONG).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionEdit:
+                final Intent goToEditActivity = new Intent(ServiceOrderListActivity.this, ServiceOrderActivity.class);
+                final ServiceOrder serviceOrder = mServiceOrdersAdapter.getSelectedItem();
+                goToEditActivity.putExtra(ServiceOrderActivity.EXTRA_SERVICE_ORDER, serviceOrder);
+                goToEditActivity.putExtra(ServiceOrderActivity.EXTRA_START_BENCHMARK, SystemClock.elapsedRealtime());
+                super.startActivityForResult(goToEditActivity, REQUEST_CODE_EDIT);
+                return true;
+            case R.id.actionDelete:
+                //TODO
+                return true;
+            default:
+                return false;
+        }
+    }
+}
---
+++ FAL8/MyApplication/app/src/main/java/com/example/administrador/myapplication/models/entities/ServiceOrder.java
@@ -0,0 +1,169 @@
+package com.example.administrador.myapplication.models.entities;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.example.administrador.myapplication.models.persistence.ServiceOrdersRepository;
+
+import java.util.Date;
+import java.util.List;
+
+public class ServiceOrder implements Parcelable {
+
+    private Integer mId;
+    private String mClient;
+    private String mAddress;
+    private Date mDate;
+    private double mValue;
+    private boolean mPaid;
+    private String mDescription;
+
+    public ServiceOrder() {
+        super();
+    }
+
+    public Integer getId() {
+        return mId;
+    }
+
+    public void setId(Integer id) {
+        this.mId = id;
+    }
+
+    public String getClient() {
+        return mClient;
+    }
+
+    public void setClient(String client) {
+        this.mClient = client;
+    }
+
+    public String getAddress() {
+        return mAddress;
+    }
+
+    public void setAddress(String address) {
+        this.mAddress = address;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        this.mDate = date;
+    }
+
+    public double getValue() {
+        return mValue;
+    }
+
+    public void setValue(double value) {
+        this.mValue = value;
+    }
+
+    public boolean isPaid() {
+        return mPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.mPaid = paid;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        this.mDescription = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ServiceOrder that = (ServiceOrder) o;
+
+        if (Double.compare(that.mValue, mValue) != 0) return false;
+        if (mPaid != that.mPaid) return false;
+        if (mId != null ? !mId.equals(that.mId) : that.mId != null) return false;
+        if (mClient != null ? !mClient.equals(that.mClient) : that.mClient != null) return false;
+        if (mAddress != null ? !mAddress.equals(that.mAddress) : that.mAddress != null) return false;
+        if (mDate != null ? !mDate.equals(that.mDate) : that.mDate != null) return false;
+        return !(mDescription != null ? !mDescription.equals(that.mDescription) : that.mDescription != null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = mId != null ? mId.hashCode() : 0;
+        result = 31 * result + (mClient != null ? mClient.hashCode() : 0);
+        result = 31 * result + (mAddress != null ? mAddress.hashCode() : 0);
+        result = 31 * result + (mDate != null ? mDate.hashCode() : 0);
+        temp = Double.doubleToLongBits(mValue);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        result = 31 * result + (mPaid ? 1 : 0);
+        result = 31 * result + (mDescription != null ? mDescription.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ServiceOrder{" +
+                "id=" + mId +
+                ", client='" + mClient + '\'' +
+                ", address='" + mAddress + '\'' +
+                ", date=" + mDate +
+                ", value=" + mValue +
+                ", paid=" + mPaid +
+                ", description='" + mDescription + '\'' +
+                '}';
+    }
+
+    public static List<ServiceOrder> getAll() {
+        return ServiceOrdersRepository.getInstance().getAll();
+    }
+
+    public void save() {
+        ServiceOrdersRepository.getInstance().save(this);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeValue(this.mId);
+        dest.writeString(this.mClient);
+        dest.writeString(this.mAddress);
+        dest.writeLong(mDate != null ? mDate.getTime() : -1);
+        dest.writeDouble(this.mValue);
+        dest.writeByte(mPaid ? (byte) 1 : (byte) 0);
+        dest.writeString(this.mDescription);
+    }
+
+    private ServiceOrder(Parcel in) {
+        this.mId = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.mClient = in.readString();
+        this.mAddress = in.readString();
+        long tmpDate = in.readLong();
+        this.mDate = tmpDate == -1 ? null : new Date(tmpDate);
+        this.mValue = in.readDouble();
+        this.mPaid = in.readByte() != 0;
+        this.mDescription = in.readString();
+    }
+
+    public static final Parcelable.Creator<ServiceOrder> CREATOR = new Parcelable.Creator<ServiceOrder>() {
+        public ServiceOrder createFromParcel(Parcel source) {
+            return new ServiceOrder(source);
+        }
+
+        public ServiceOrder[] newArray(int size) {
+            return new ServiceOrder[size];
+        }
+    };
+}
